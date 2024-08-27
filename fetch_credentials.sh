07798ba80b244869df0701478fa934743d2a2157
Viren Khanna <109760972+Viren0602@users.noreply.github.com>
2024-08-27 11:11:43 +0530

    Update fetch_credentials.sh

--- fetch_credentials.sh
+++ fetch_credentials.sh
@@ -2,23 +2,18 @@
 
 
 
-#!/bin/sh
-
-# Set Vault environment variables
-export VAULT_ADDR='http://192.168.1.51:8200'
+# Set up Vault environment variables
+export VAULT_ADDR='http://192.168.3.174:8200'
 export VAULT_TOKEN='test'
 
-# Fetch credentials from Vault
-USERNAME=$(vault kv get -field=username secret/myapp)
-PASSWORD=$(vault kv get -field=password secret/myapp)
+# Retrieve secrets from Vault
+screener_username=$(vault kv get -field=username secret/data/myapp)
+screener_password=$(vault kv get -field=password secret/data/myapp)
 
-# Export these variables so they can be used in Concourse tasks
-export VAULT_USERNAME=$USERNAME
-export VAULT_PASSWORD=$PASSWORD
+# Print retrieved secrets
+echo "Retrieved Screener Username: $screener_username"
+echo "Retrieved Screener Password: $screener_password"
 
-# Optional: Print the fetched credentials (for debugging)
-echo "Fetched Username: $VAULT_USERNAME"
-echo "Fetched Password: $VAULT_PASSWORD"
 
 
 
